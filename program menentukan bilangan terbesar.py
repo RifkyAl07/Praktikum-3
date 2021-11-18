9aa1159c0234e83148f23203e03e169100062976
RifkyAl07 <92801053+RifkyAl07@users.noreply.github.com>
2021-11-18 19:14:23 +0700

    Add files via upload

---
+++ program menentukan bilangan terbesar.py
@@ -0,0 +1,15 @@
+# program menentukan bilangan terbesar
+
+print(20*"=")
+
+a =int(input("masukan bilangan ke 1 = "))
+b =int(input("masukan bilangan ke 2 = "))
+
+if a > b :
+    print("bilangan ke 1 yang terbesar")
+elif b > a :
+    print("bilangan ke 2 yang terbesar")
+else:
+    print("kesalahan")
+
+print(20*"=")
